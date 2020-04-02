55b8049ac521cbca750bc5693cd8d5d122901abc
pratiksha <pratikshataank19@gmail.com>
2020-04-02 10:35:46 +0530

    commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+             return View();
+        }
+
+        public ActionResult Contact()
+        {
+             return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Weeding()
+        {
+         return View();
+        }
+
+        public ActionResult Birthday()
+        {
+            return View();
+        }
+
+        public ActionResult sports()
+        {
+            return View();
+        }
+
+        public ActionResult Engagement()
+        {
+            return View();
+        }
+
+        public ActionResult Cradle()
+        {
+            return View();
+        }
+
+        public ActionResult Anniversary()
+        {
+            return View();
+        }
+
+        public ActionResult slider()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+    }
+}
